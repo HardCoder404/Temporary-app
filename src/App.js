81c474492272ca31f553dd6390d74dcf4ffa7199
Prince Raj <praj81232@gmail.com>
2024-01-17 19:56:53 +0530

    add one more line

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Temporary-app
+        Temporary-app (ab maine change krida)
         </p>
         <a
           className="App-link"
